da38bab47ab49a91da42b661ab19c4df9ea3bdd8
desknoc <juangamering82@gmail.com>
2026-06-21 19:40:09 -0500

    Hacemos los endpoints de EliminarNovedad por id

--- novedades/urls.py
+++ novedades/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.Home),
     path('editar/<int:id>/', views.EditarNovedad), #Buscamos por id
-    path('crearnovedad/', views.CrearNovedad)
+    path('crearnovedad/', views.CrearNovedad),
+    path('eliminarnovedad/<int:id>/', views.EliminarNovedad) #Buscamos por id
 ]
